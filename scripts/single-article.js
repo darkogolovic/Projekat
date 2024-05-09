4358355199619a86d72b915e07f055e44d475afc
Darko Golovic <golovicdarko@gmail.com>
2024-05-09 23:21:37 +0200

    cart finished

--- scripts/single-article.js
+++ scripts/single-article.js
@@ -7,8 +7,6 @@ const productData = JSON.parse(decodeURIComponent(productDataString));
 const { brand, model, price, sizes, images } = productData;
 const articleContainer = document.querySelector(".article-container");
 
-console.log(productData);
-
 // Prikazati proizvod na stranici
 
 articleContainer.innerHTML = `
@@ -27,9 +25,9 @@ articleContainer.innerHTML = `
             </ul>
           </div>
 
-          <p class="price">$${price}</p>
+          <p class="price">&euro; ${price}</p>
 
-          <button class="add-to-cart">Add to cart</button>
+          <button class="add-to-cart " id="add">Add to cart</button>
         </div>`;
 
 const switchPicture = document.querySelectorAll(".article-images img");
@@ -49,6 +47,7 @@ document.querySelector(".modal").addEventListener("click", closeModal);
 window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     closeModal();
+    cartModal.style.display = "none";
   }
 });
 
@@ -63,3 +62,95 @@ function closeModal() {
   let modal = document.getElementById("myModal");
   modal.style.display = "none";
 }
+
+// Cart modal
+
+const cart = document.querySelector(".cart");
+const addBtn = document.querySelector("#add");
+const cartItem = document.querySelector("#cart-item");
+const totalPrice = document.querySelector("#total-price");
+cartItem.innerHTML = localStorage.getItem("cartItem");
+totalPrice.innerHTML = "&euro;" + " " + localStorage.getItem("count");
+const cartIcon = document.querySelector(".cart-icon");
+const cartModal = document.querySelector(".cart-modal");
+const cartClose = document.querySelector(".cart-close");
+
+cartIcon.addEventListener("click", () => {
+  cartModal.style.display = "block";
+  if (
+    localStorage.getItem("itemCounter") == 0 ||
+    !localStorage.getItem("itemCounter")
+  ) {
+    cartItem.innerHTML = `<h2>Your cart is empty</h2>`;
+  }
+});
+
+cartIcon.addEventListener("click", () => {
+  cart.style.display = "block";
+});
+
+let count = Number(localStorage.getItem("count")) || 0;
+let itemCounter = Number(localStorage.getItem("itemCounter")) || 0;
+
+cartIcon.setAttribute("data-count", itemCounter);
+
+addBtn.addEventListener("click", () => {
+  cartItem.innerHTML += `<div class="cart-item">
+  <div class="cart-img">
+    <img src="${images[0]}" alt="${brand}" />
+  </div>
+  <p>${brand} - ${model}</p>
+  <h2>&euro; ${price}.00</h2>
+  <i class="fa-solid fa-trash-can"></i>
+</div> `;
+  addTrashListeners();
+  itemCounter++;
+  localStorage.setItem("itemCounter", itemCounter);
+  cartIcon.setAttribute("data-count", itemCounter);
+  count = count + price;
+  console.log(count);
+  localStorage.setItem("count", count);
+  totalPrice.innerHTML = "&euro;" + " " + count;
+  localStorage.setItem("cartItem", cartItem.innerHTML);
+});
+
+const addTrashListeners = function () {
+  cartItem.querySelectorAll(".fa-trash-can").forEach((el) => {
+    el.addEventListener("click", (e) => {
+      itemCounter--;
+
+      cartIcon.setAttribute("data-count", itemCounter);
+      localStorage.setItem("itemCounter", itemCounter);
+
+      e.currentTarget.parentElement.remove();
+      count = count - price;
+      console.log(count);
+      if (count <= 0) {
+        count = 0;
+        cartItem.innerHTML = `<h2>Your cart is empty</h2>`;
+      }
+      totalPrice.innerHTML = "&euro;" + " " + count;
+
+      localStorage.setItem("cartItem", cartItem.innerHTML);
+      localStorage.setItem("count", count);
+    });
+  });
+};
+
+cartClose.addEventListener("click", () => {
+  cartModal.style.display = "none";
+});
+window.addEventListener("click", (e) => {
+  if (e.target == cartModal) {
+    cartModal.style.display = "none";
+  }
+});
+
+/* <div class="cart-item">
+  <div class="cart-img">
+    <img src="${images[0]}" alt="${brand}" />
+  </div>
+  <p>${brand} - ${model}</p>
+  <h2>euro ${price}.00</h2>
+  <i class="fa-solid fa-trash-can"></i>
+</div> */
